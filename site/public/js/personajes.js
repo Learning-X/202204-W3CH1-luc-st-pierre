89c1b2177532937937549e2adad602b9590e9e18
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-18 21:41:19 +0200

    Created initPersonajes function to export into index entry point

--- site/public/js/personajes.js
+++ site/public/js/personajes.js
@@ -3,12 +3,16 @@ import Escudero from "./Escudero.js";
 import Luchador from "./Luchador.js";
 import Rey from "./Rey.js";
 
-const personajes = [];
+const initPersonajes = () => {
+  const personajes = [];
 
-const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
-const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
-const daenerys = new Luchador("Daenerys", "Targaryen", 30, "Dragones", 10);
-const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
-const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
+  const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
+  const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
+  const daenerys = new Luchador("Daenerys", "Targaryen", 30, "Dragones", 10);
+  const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
+  const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
 
-personajes.push(joffrey, jaime, daenerys, tyrion, bronn);
+  personajes.push(joffrey, jaime, daenerys, tyrion, bronn);
+};
+
+export default initPersonajes;
