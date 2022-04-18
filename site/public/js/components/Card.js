5d98be42cd2e85f19bcb0db471a7c80fa4a5d943
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-18 22:10:11 +0200

    Created render method and extracted character__card markup content

--- site/public/js/components/Card.js
+++ site/public/js/components/Card.js
@@ -1,9 +1,52 @@
 import Component from "./Component";
 
 // 1. will start by creating a Card component
-
+// 2. will need to render content
+// 3. will need a personaje object for character__card
 class Card extends Component {
   constructor(parentElement, className, htmlTag) {
     super(parentElement, className, htmlTag);
+
+    this.render();
+  }
+
+  render() {
+    this.element.innerHtml = `
+      <div class="card character__card">
+            <img
+              src="img/no-one.jpeg"
+              alt="Nombre y familia del personaje"
+              class="character__picture card-img-top"
+            />
+            <div class="card-body">
+              <h2 class="character__name card-title h4">Nombre y familia</h2>
+              <div class="character__info">
+                <ul class="list-unstyled">
+                  <li>Edad: X años</li>
+                  <li>
+                    Estado:
+                    <i class="fas fa-thumbs-down"></i>
+                    <i class="fas fa-thumbs-up"></i>
+                  </li>
+                </ul>
+              </div>
+              <div class="character__overlay">
+                <ul class="list-unstyled">
+                  <li>Años de reinado: X</li>
+                  <li>Arma: XXX</li>
+                  <li>Destreza: X</li>
+                  <li>Peloteo: X</li>
+                  <li>Asesora a: X</li>
+                  <li>Sirve a: X</li>
+                </ul>
+                <div class="character__actions">
+                  <button class="character__action btn">habla</button>
+                  <button class="character__action btn">muere</button>
+                </div>
+              </div>
+            </div>
+            <i class="emoji"></i>
+          </div>
+    `;
   }
 }
