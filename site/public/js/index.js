7474dd938c4b778c75e113cd4e6869a42979a491
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-18 23:03:14 +0200

    Created new Card instance for each personajes and passed  object

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,3 +1,12 @@
+import Card from "./components/Card.js";
 import initPersonajes from "./personajes.js";
 
-initPersonajes();
+// 1. will need to add each new Personaje to DOM to a parent container
+// 2. will need to destructure {personaje}
+
+const personajes = initPersonajes();
+const liContainer = document.querySelector(".app.container ul");
+
+personajes.forEach((personaje) => {
+  const card = new Card(liContainer, "character col", "li", personaje);
+});
