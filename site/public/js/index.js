9bee6cad8676275961cfd3b91efd2733482b3acf
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-19 00:15:39 +0200

    Destructured personaje in Card component

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -2,7 +2,7 @@ import Card from "./components/Card.js";
 import initPersonajes from "./personajes.js";
 
 // 1. will need to add each new Personaje to DOM to a parent container
-// 2. will need to destructure {personaje}
+// 2. will need to pass personaje to Card component
 
 const personajes = initPersonajes();
 const liContainer = document.querySelector(".app.container ul");
