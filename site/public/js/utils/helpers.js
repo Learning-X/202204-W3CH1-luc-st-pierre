a858e0494fbfccf824bd9f63f5faa817f2c7a660
Learning X <luc.st-pierre@protonmail.com>
2022-04-19 22:42:39 +0200

    Fixed [object Object] string return and add static to subclass Escudero

--- site/public/js/utils/helpers.js
+++ site/public/js/utils/helpers.js
@@ -1,10 +1,14 @@
+import Asesor from "../personajes/Asesor.js";
+import Escudero from "../personajes/Escudero.js";
+
 import Personaje from "./Personaje.js";
 
 // 1. Got properties from Personaje class
 // 2. Got to get own properties for each instance personajes
 // 3. Renamed name variables and function for confusion,
 // 4. Filtered personaje own properties from Personaje class
-// 5.
+// 5. Mapped personajeProperties to return a string for each personaje
+// 6. Need to get
 
 const getPersonajeProperties = (personaje) => {
   const personajeClassProperties = Object.getOwnPropertyNames(personaje);
@@ -19,11 +23,21 @@ const getPersonajeProperties = (personaje) => {
   );
 
   const personajeAttributes = personajeProperties.map((personajeProperty) => {
+    if (
+      ((personaje instanceof Escudero || personaje instanceof Asesor) &&
+        personajeProperty === "asesorado") ||
+      personajeProperty === "sirveA"
+    ) {
+      // console.log(
+      //   `${personajeProperty}: ${personaje[personajeProperty].nombre}`
+      // );
+      return `${personajeProperty}: ${personaje[personajeProperty].nombre}`;
+    }
     return `${personajeProperty}: ${personaje[personajeProperty]}`;
   });
 
   // console.log(personaje);
-  // console.log(personajeAttributes);
+  console.log(personajeAttributes);
   return personajeAttributes;
 };
 
