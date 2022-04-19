6b6c16addd0a017cb0a7a4818b3db4b1c2cfe4ed
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-19 12:51:56 +0200

    Did some renaming and filtered personaje for own properties

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,7 +1,7 @@
 import Card from "./components/Card.js";
 import Communication from "./components/Communication.js";
 import initPersonajes from "./personajes.js";
-import getPersonajesProperties from "./utils/helpers.js";
+import getPersonajeProperties from "./utils/helpers.js";
 
 // 1. will need to add each new Personaje to DOM to a parent container
 // 2. will need to pass personaje to Card component
@@ -12,6 +12,6 @@ const communicator = new Communication(document.body, "div", "comunication");
 
 personajes.forEach((personaje) => {
   const card = new Card(liContainer, "character col", "li", personaje);
-  const properties = getPersonajesProperties(personaje);
-  // console.log(properties);
+  const properties = getPersonajeProperties(personaje);
+  console.log(properties);
 });
