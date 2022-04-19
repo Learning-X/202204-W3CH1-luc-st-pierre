3d274072b2101e9773c4f66192056667d3012fbf
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-19 08:40:11 +0200

    Extracted html markup content for cummunication component

--- site/public/js/components/Communication.js
+++ site/public/js/components/Communication.js
@@ -6,4 +6,15 @@ export default class ButtonComponent extends Component {
 
     this.render();
   }
+
+  render() {
+    this.element.innerHTML = `
+      <p class="comunications__text display-1">Una frase que dice alguien</p>
+      <img
+        class="comunications__picture"
+        src="img/no-one.jpeg"
+        alt="Nombre y familia del que habla"
+      />
+    `;
+  }
 }
