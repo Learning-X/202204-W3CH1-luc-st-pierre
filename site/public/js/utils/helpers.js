906d4e3c206677d09280537855014929e82f0e24
Learning X <luc.st-pierre@protonmail.com>
2022-04-19 22:12:58 +0200

    Mapped personajeProperties to return a string for each personaje

--- site/public/js/utils/helpers.js
+++ site/public/js/utils/helpers.js
@@ -3,22 +3,28 @@ import Personaje from "./Personaje.js";
 // 1. Got properties from Personaje class
 // 2. Got to get own properties for each instance personajes
 // 3. Renamed name variables and function for confusion,
-// 4  Extracted, (filter) personaje own properties from Personaje class
+// 4. Filtered personaje own properties from Personaje class
+// 5.
 
 const getPersonajeProperties = (personaje) => {
   const personajeClassProperties = Object.getOwnPropertyNames(personaje);
 
   const instancePersonaje = new Personaje();
+
   const instancePersonajeProperties =
     Object.getOwnPropertyNames(instancePersonaje);
 
-  // need to Not include properties from personajeClassProperties
   const personajeProperties = personajeClassProperties.filter(
     (prop) => !instancePersonajeProperties.includes(prop)
   );
 
-  // console.log(props);
-  return personajeProperties;
+  const personajeAttributes = personajeProperties.map((personajeProperty) => {
+    return `${personajeProperty}: ${personaje[personajeProperty]}`;
+  });
+
+  // console.log(personaje);
+  // console.log(personajeAttributes);
+  return personajeAttributes;
 };
 
 export default getPersonajeProperties;
