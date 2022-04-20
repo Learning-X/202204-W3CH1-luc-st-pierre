63c2315db44761f09ef7a425af1e9a33a2e10e4a
Learning X <luc.st-pierre@protonmail.com>
2022-04-20 03:23:30 +0200

    Still have button call action issues

--- site/public/js/components/Communication.js
+++ site/public/js/components/Communication.js
@@ -12,7 +12,7 @@ export default class Communication extends Component {
       <p class="comunications__text display-1">Una frase que dice alguien</p>
       <img
         class="comunications__picture"
-        src="img/no-one.jpeg"
+        src="img/no-one.jpg"
         alt="Nombre y familia del que habla"
       />
     `;
